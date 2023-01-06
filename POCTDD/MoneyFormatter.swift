00271f95b76c730d77c0c61f87fcb2d5781a3460
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2023-01-06 12:49:42 -0300

    beggining with tdd

---
+++ POCTDD/MoneyFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  MoneyFormatter.swift
+//  POCTDD
+//
+//  Created by Lucas Barbosa de Oliveira on 06/01/23.
+//
+
+import Foundation
--- POCTDD/ViewController.swift
+++ POCTDD/ViewController.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Lucas Barbosa de Oliveira on 05/01/23.
 //
+// Learning and documentation of TDD (Test Drive Devolopment)
+// the TDD method begins with the test before the code  going against the pattern
 
 import UIKit
 
