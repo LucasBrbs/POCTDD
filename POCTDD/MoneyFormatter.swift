086e8170563871801af8409649c50f1814f5ab93
Ieda Xavier <92699834+Sunlower@users.noreply.github.com>
2023-01-07 18:47:20 -0300

    final implentation of the first class of tdd

--- POCTDD/MoneyFormatter.swift
+++ POCTDD/MoneyFormatter.swift
@@ -7,14 +7,28 @@
 
 import Foundation
 
-struct MoneyFormatter {
+extension Decimal {
 
-    func string(decimal: Decimal) -> String {
+    var moneyString: String {
         let numberFormatter = NumberFormatter()
         numberFormatter.minimumFractionDigits = 2
         numberFormatter.roundingMode = .floor
-        return numberFormatter.string(from: decimal as NSDecimalNumber) ?? ""
+//        return numberFormatter.string(from: decimal as NSDecimalNumber) ?? ""
+        return numberFormatter.string(from: self as NSDecimalNumber) ?? ""
 
     }
 
 }
+
+
+//struct MoneyFormatter {
+//
+//    func string(decimal: Decimal) -> String {
+//        let numberFormatter = NumberFormatter()
+//        numberFormatter.minimumFractionDigits = 2
+//        numberFormatter.roundingMode = .floor
+//        return numberFormatter.string(from: decimal as NSDecimalNumber) ?? ""
+//
+//    }
+//
+//}
--- POCTDDTests/POCTDDTests.swift
+++ POCTDDTests/POCTDDTests.swift
@@ -10,22 +10,25 @@ import XCTest
 
 final class POCTDDTests: XCTestCase {
 
-    let formatter = MoneyFormatter()
+//    let formatter = MoneyFormatter()
 
     func testMoneyFormatterWholeNumber(){
-        XCTAssertEqual(formatter.string(decimal: Decimal(string : "0")!), "0,00")
-        XCTAssertEqual(formatter.string(decimal: Decimal(string : "1")!), "1,00")
+        XCTAssertEqual(Decimal(string : "0")?.moneyString, "0,00")
+//        XCTAssertEqual(formatter.string(decimal: Decimal(string : "1")!), "1,00")
     }
 
     func testMoneyFormatterDecimalNumberWithOneDecimalPlace() {
-        XCTAssertEqual(formatter.string(decimal: Decimal(string : "1.1")!), "1,10")
+        XCTAssertEqual(Decimal(string : "1.1")?.moneyString, "1,10")
+//        XCTAssertEqual(formatter.string(decimal: Decimal(string : "1.1")!), "1,10")
     }
 
     func testMoneyFormatterDecimalNumberWithTwoDecimalPlace() {
-        XCTAssertEqual(formatter.string(decimal: Decimal(string : "1.11")!), "1,11")
+        XCTAssertEqual(Decimal(string : "1.11")?.moneyString, "1,11")
+//        XCTAssertEqual(formatter.string(decimal: Decimal(string : "1.11")!), "1,11")
     }
 
     func testMoneyFormatterDecimalNumberWithAnyDecimalPlace() {
-        XCTAssertEqual(formatter.string(decimal: Decimal(string : "1.111")!), "1,11")
+        XCTAssertEqual(Decimal(string : "1.111")?.moneyString, "1,11")
+//        XCTAssertEqual(formatter.string(decimal: Decimal(string : "1.111")!), "1,11")
     }
 }
