e4dfb58de72e002cf6d1facbcf07e15b66e4b23a
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2023-01-19 14:08:35 -0300

    final lesson class 3

--- POCTDD/ViewController.swift
+++ POCTDD/ViewController.swift
@@ -12,12 +12,44 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var todayRemainingLabel: UILabel!
+    @IBOutlet weak var weeklyRemainingLabel: UILabel!
+    @IBOutlet weak var transactionTextField: UITextField!
+    @IBOutlet weak var totalBudgetTextField: UITextField!
+
+    var budget:Budget?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    private func updateBudgetLabels(){
+        weeklyRemainingLabel.text = budget?.weeklyRemaining.moneyString
+        todayRemainingLabel.text = budget?.dailyRemaining.moneyString
+    }
+}
 
+extension ViewController:UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let total = Decimal(string: textField.text!) else {
+            return true
+        }
+
+        if textField == totalBudgetTextField {
+            budget = Budget(total: total )
+//            weeklyRemainingLabel.text = budget?.weeklyRemaining.moneyString
+//            todayRemainingLabel.text = budget?.dailyRemaining.moneyString
+        }
+
+        if textField == transactionTextField {
+            budget?.addTransaction(amount: total, timestamp: Date())
+//            weeklyRemainingLabel.text = budget?.weeklyRemaining.moneyString
+//            todayRemainingLabel.text = budget?.dailyRemaining.moneyString
+        }
+        updateBudgetLabels()
+        //with this function is no more necessary the repetition in this previuos lines of code 
+        return true
+    }
 }
 
--- POCTDDTests/ViewControllerTests.swift
+++ POCTDDTests/ViewControllerTests.swift
@@ -6,9 +6,44 @@
 //
 
 import XCTest
+@testable import POCTDD
 
 final class ViewControllerTests: XCTestCase {
 
-    
+    var viewControlller: ViewController!
 
+    override func setUp() {
+        let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as? ViewController
+        _ = viewController.view
+    }
+
+    func testCreatingANewBudgetUpdatesRemainingLabels(){
+//        let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! ViewController
+//        _ = viewController.view
+        //on isolated cases
+
+        let textField = viewControlller.totalBudgetTextField!
+        textField.text = "100"
+        _ = textField.delegate?.textFieldShouldReturn?(textField)
+
+        let budget = viewControlller.budget
+        XCTAssertEqual(viewControlller.weeklyRemainingLabel.text, budget?.weeklyRemaining.moneyString)
+        XCTAssertEqual(viewControlller.todayRemainingLabel.text, budget?.dailyRemaining.moneyString)
+
+    }
+    func testCreatingTransactionUpdatesRemainingLabels(){
+        let budget = Budget(total: 70)
+        budget.addTransaction(amount: 5, timestamp: Date())
+
+        let textField = viewControlller.totalBudgetTextField!
+        textField.text = "70"
+        _ = textField.delegate?.textFieldShouldReturn?(textField)
+
+        let textField2 = viewControlller.transactionTextField!
+        textField2.text = "5"
+        _ = textField2.delegate?.textFieldShouldReturn?(textField2)
+
+        XCTAssertEqual(viewControlller.weeklyRemainingLabel.text, budget.weeklyRemaining.moneyString)
+        XCTAssertEqual(viewControlller.todayRemainingLabel.text, budget.dailyRemaining.moneyString)
+    }
 }
